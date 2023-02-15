d84d3d5e94116a0a30b1c8c36b1c24267b70d08a
coderpr0grammer <daniellhf@icloud.com>
2023-02-15 09:54:46 -0500

    Initial commit

---
+++ helloWorld.java
@@ -0,0 +1,5 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! I'm a computer program, but please don't hold that against me. I may not have feelings, but I do have a great sense of humor. For example, did you hear the one about the programmer who got stuck in the shower? He couldn't find the 'shampoo' bottle because the instructions were in C++. Ha! Alright, that's enough jokes for now. But seriously, it's nice to meet you.");
+    }
+}
