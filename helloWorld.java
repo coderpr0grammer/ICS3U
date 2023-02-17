2a13a1849021fb9e7617b8316494db7e5876f4b1
coderpr0grammer <daniellhf@icloud.com>
2023-02-17 15:56:52 -0500

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,10 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Daniel Martinez");
+        System.out.println("Ms. Katsman");
+        System.out.println("Computer Science");
+        System.out.println("Grade 11 Functions");
+        System.out.println("Science");
+        System.out.println("Core French");
     }
 }
