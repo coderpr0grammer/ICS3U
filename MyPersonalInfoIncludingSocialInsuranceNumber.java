9128aa20d15a4fce516a38e7896df158a3ed1d96
Daniel Martinez <daniellhf@icloud.com>
2023-02-20 22:06:42 -0500

    update file

--- MyPersonalInfoIncludingSocialInsuranceNumber.java
+++ MyPersonalInfoIncludingSocialInsuranceNumber.java
@@ -1,4 +1,4 @@
-public class helloWorld {
+public class MyPersonalInfoIncludingSocialInsuranceNumber {
     public static void main(String[] args) {
         System.out.println("Daniel Martinez");
         System.out.println("Ms. Katsman");
@@ -6,5 +6,6 @@ public class helloWorld {
         System.out.println("Grade 11 Functions");
         System.out.println("Science");
         System.out.println("Core French");
+        System.out.println("Favorite quote: 'If you are making mistakes, try not to make mistakes' -My Gr10 math teacher");
     }
 }
