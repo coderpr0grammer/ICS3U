3320346f425280f3011a2314e3f689a6750d8397
coderpr0grammer <daniellhf@icloud.com>
2023-04-18 08:53:44 -0400

    Create StringInvestigation.java

---
+++ StringInvestigation.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StringInvestigation {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+System.out.println(name);
+
+    }
+}
