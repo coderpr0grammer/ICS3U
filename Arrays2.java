ae5dda3b75d668529ca8bb8a9b24696422ccfb5a
coderpr0grammer <daniellhf@icloud.com>
2023-05-10 08:57:10 -0400

    Create Arrays2.java

---
+++ Arrays2.java
@@ -0,0 +1,13 @@
+public class Arrays2 {
+    public static void main (String[] args) {
+        int[] numbers = { 1, 2, 3 };
+
+
+
+        int[] newNumbers = numbers.clone();
+
+        
+
+        System.out.println(newNumbers[2]);
+    }
+}
