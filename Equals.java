099a2f6d9bb6ff30773dd4173d39e51a652eb109
Daniel Martinez <daniellhf@icloud.com>
2023-06-28 09:21:56 -0400

    Create Equals.java

---
+++ Equals.java
@@ -0,0 +1,9 @@
+public class Equals {
+
+    public static String equals() {
+        return "equals";
+    }
+    public static void main(String[] args) {
+        System.out.println(equals()== "equals");
+    }
+}
